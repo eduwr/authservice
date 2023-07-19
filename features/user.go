f6692551adb45679e6afec94ce7a3253879c241f
Eduardo Ricardo <eduwric@gmail.com>
2023-07-19 16:08:07 -0300

    feat: :sparkles: create auth methods

---
+++ features/user.go
@@ -0,0 +1,108 @@
+package user
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type User struct {
+	email        string
+	username     string
+	passwordhash string
+	fullname     string
+	createDate   string
+	role         int
+}
+
+// TODO: use a real database instead
+var userList []User
+
+func GetUserObject(email string) (User, bool) {
+	for _, user := range userList {
+		if user.email == email {
+			return user, true
+		}
+	}
+
+	return User{}, false
+}
+
+func (u *User) ValidatePasswordHash(pswdhash string) bool {
+	return u.passwordhash == pswdhash
+}
+
+func AddUserObject(email string, username string, passwordhash string, fullname string, role int) bool {
+	newUser := User{
+		email:        email,
+		passwordhash: passwordhash,
+		username:     username,
+		fullname:     fullname,
+		role:         role,
+	}
+
+	for _, ele := range userList {
+		if ele.email == email || ele.username == username {
+			return false
+		}
+	}
+	userList = append(userList, newUser)
+	return true
+}
+
+func GenerateToken(header string, payload map[string]string, secret string) (string, error) {
+	h := hmac.New(sha256.New, []byte(secret))
+	header64 := base64.StdEncoding.EncodeToString([]byte(header))
+
+	payloadstr, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error generating token")
+		return string(payloadstr), err
+	}
+
+	payload64 := base64.StdEncoding.EncodeToString([]byte(payloadstr))
+
+	message := header64 + "." + payload64
+
+	unsignedStr := header + string(payloadstr)
+
+	h.Write([]byte(unsignedStr))
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+	tokenStr := message + "." + signature
+	return tokenStr, nil
+}
+
+func validateToken(token string, secret string) (bool, error) {
+	splitToken := strings.Split(token, ".")
+	if len(splitToken) != 3 {
+		return false, nil
+	}
+
+	header, err := base64.StdEncoding.DecodeString(splitToken[0])
+	if err != nil {
+		return false, nil
+	}
+
+	payload, err := base64.StdEncoding.DecodeString(splitToken[1])
+	if err != nil {
+		return false, nil
+	}
+
+	unsignedStr := string(header) + string(payload)
+
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(unsignedStr))
+
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	fmt.Println(signature)
+
+	if signature != splitToken[2] {
+		return false, nil
+	}
+
+	return true, nil
+}
