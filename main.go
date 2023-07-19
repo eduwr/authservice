5bff817f5c6926af5ab97fdec26ba5e54d69a8b8
Eduardo Ricardo <eduwric@gmail.com>
2023-07-19 17:04:35 -0300

    feat: :sparkles: last commit

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"authservice/src/handlers"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("HELLO GO")
+	mainRouter := mux.NewRouter()
+	authRouter := mainRouter.PathPrefix("/auth").Subrouter()
+	authRouter.HandleFunc("/signup", handlers.SignupHandler)
+	authRouter.HandleFunc("/signin", handlers.SigninHandler)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:9090",
+		Handler: mainRouter,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic("Error Booting server")
+	}
+
+	fmt.Println("Server running in port 9090")
 }
